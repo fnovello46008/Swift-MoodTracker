ca8f2e7242b105fa0d34418b74cc1e3df7a13e64
fnovello46008 <fnovello46008@Gmail.com>
2020-10-30 22:49:12 -0400

    Initial commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Frank Novello on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func ExcitedButton(_ sender: Any) {
+        
+        print("hello")
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
